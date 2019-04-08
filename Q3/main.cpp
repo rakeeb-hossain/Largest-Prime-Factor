f01f69669dc5b20c15edf096c5b4af2c776e5318
Rakeeb <Rakeeb@MacBook-Pro.local>
2019-04-08 00:23:17 -0400

    Initial Commit

---
+++ Q3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Q3
+//
+//  Created by Rakeeb on 2019-04-08.
+//  Copyright © 2019 Rakeeb. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
